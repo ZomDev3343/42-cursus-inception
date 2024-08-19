5f6f96209e90cd76869fc1f1e9e338bea76d99b3
zom <contact.zomzd@gmail.com>
2024-08-19 14:04:32 +0200

    gjejg

--- pre_launch.sh
+++ pre_launch.sh
@@ -1,9 +1,9 @@
 if [ -d "/home/truello/data/wordpress" ]; then \
-	echo "/data/wordpress already exists"; else \
+	echo "/home/truello/data/wordpress already exists"; else \
 	sudo mkdir -p /home/truello/data/wordpress; \
 fi
 
 if [ -d "/home/truello/data/mariadb" ]; then \
-	echo "/data/mariadb already exists"; else \
+	echo "/home/truello/data/mariadb already exists"; else \
 	sudo mkdir -p /home/truello/data/mariadb; \
 fi
