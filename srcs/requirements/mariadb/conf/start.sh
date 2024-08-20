f9efca9c320d9fe3de06eadacb6e4685846d4c0a
Zom' / Thomas <contact.zomzd@gmail.com>
2024-08-20 12:23:25 +0200

    Update start.sh

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,7 +1,6 @@
 service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER_ADMIN}\`@'localhost' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';"
-mysql -e "CREATE TABLE test(value int, name varchar(50));"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER_ADMIN}\`@'%' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin shutdown
